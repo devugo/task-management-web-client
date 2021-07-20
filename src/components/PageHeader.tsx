0edc1c85b4b2f993e01603ef584d09afa166e322
Ezenwankwo Ugochukwu <ugoezenwankwo@gmail.com>
2021-07-20 16:14:26 +0100

    Add animation to home page

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom';
+
 import LogoWhite from './LogoWhite';
 
 const PageHeader = () => {
   return (
     <div className="page-header">
-      <LogoWhite width={26} height={26} />
-      <h1> Management</h1>
+      <Link to="/" className="task-management-text">
+        <LogoWhite width={26} height={26} />
+        <h1> Management</h1>
+      </Link>
     </div>
   );
 };
